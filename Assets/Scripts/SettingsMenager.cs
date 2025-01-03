51708f4366d43452a6932cfacbc7ee1a30bccdda
Weronika Litwin <weronikalitwin1601@gmail.com>
2025-01-03 15:44:54 +0100

    fixed saving options and settings panel, sounds and game over panel to be fixed
    saving options fixed, settings panel improved,
    game over and winner panel to be fixed
    falling sound and ghost sound to be fixed

--- Assets/Scripts/SettingsMenager.cs
+++ Assets/Scripts/SettingsMenager.cs
@@ -3,52 +3,58 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    public GameObject settingsPanel; 
-    public Toggle soundToggle;       
-    public Button backButton;        
-    public Button ballColorButton;  
+    public GameObject settingsPanel;   // Reference to the settings panel
+    public Toggle soundToggle;         // Toggle for enabling/disabling sound
+    public Slider volumeSlider;        // Slider for adjusting volume
+    public Button backButton;          // Button to return to the pause menu
+    
     private bool isSoundOn = true;
+    private int currentBallColorIndex = 0;
 
     void Start()
     {
-    
+        // Load sound settings
         isSoundOn = PlayerPrefs.GetInt("Sound", 1) == 1;
-        soundToggle.isOn = isSoundOn; 
+        soundToggle.isOn = isSoundOn;
+        AudioListener.volume = isSoundOn ? PlayerPrefs.GetFloat("Volume", 1f) : 0f;
+
+        // Load volume settings
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
+
+        // Set up UI listeners
         soundToggle.onValueChanged.AddListener(ToggleSound);
+        volumeSlider.onValueChanged.AddListener(AdjustVolume);
         backButton.onClick.AddListener(GoBackToPauseMenu);
-        ballColorButton.onClick.AddListener(ChangeBallColor); 
+        
+        // Hide settings panel initially
         settingsPanel.SetActive(false);
     }
 
     void ToggleSound(bool isOn)
     {
         isSoundOn = isOn;
+        AudioListener.volume = isSoundOn ? volumeSlider.value : 0f;
+
+        // Save sound settings
+        PlayerPrefs.SetInt("Sound", isSoundOn ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    void AdjustVolume(float volume)
+    {
         if (isSoundOn)
         {
-           
-            AudioListener.volume = 1f; 
-        }
-        else
-        {
-        
-            AudioListener.volume = 0f; 
+            AudioListener.volume = volume;
         }
 
-    
-        PlayerPrefs.SetInt("Sound", isSoundOn ? 1 : 0);
-        PlayerPrefs.Save(); 
+        // Save volume settings
+        PlayerPrefs.SetFloat("Volume", volume);
+        PlayerPrefs.Save();
     }
 
-  
+
     void GoBackToPauseMenu()
     {
-        settingsPanel.SetActive(false); 
-       
-    }
-    void ChangeBallColor()
-    {
-        // Later, implement functionality to change the ball's color
-        Debug.Log("Ball Color Change functionality is not yet implemented.");
+        settingsPanel.SetActive(false);
     }
 }
-
